eb5a7fc574cc5e46f85fddb75cade7b95f7ea3f7
Vrcck <123900151+Vrcck@users.noreply.github.com>
2023-03-22 15:16:45 +0300

    1

--- INISlab2/INIS2.js
+++ INISlab2/INIS2.js
@@ -1,5 +1,5 @@
 let personalMovieDB = {
-    privat,
+    privat: false,
     movies: {}
 };
 let filmOne;
@@ -20,3 +20,14 @@ personalMovieDB.movies[filmOne] = evOne;
 personalMovieDB.movies[filmTwo] = evTwo;
 personalMovieDB.movies[filmTree] = evTree;
 
+personalMovieDB.privat = prompt('Вы хотите посмотреть таблицу? true/false');
+
+function tabl(personalMovieDB) {
+    if (personalMovieDB.privat != true) {
+        for (const property in personalMovieDB) {
+            console.log(`${property}: ${object[property]}`);
+        }
+    }
+}
+
+console.log(personalMovieDB);
