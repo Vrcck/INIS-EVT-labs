878ca326046431519f81ff1b0a02a85f8219615e
Vrcck <123900151+Vrcck@users.noreply.github.com>
2023-12-06 14:34:40 +0300

    1

--- KG_lab2/shifr.py
+++ KG_lab2/shifr.py
@@ -2,7 +2,6 @@ key = [['q','w','e','r','t'],['y','u','i','o','p'],['a','s','d','f','g'],['h','k
 # J и I объединены в одну ячейку
 
 text = 'semyonova varvara olegovna'
-bigramm = ''
 shifr = ''
 
 import re
@@ -11,6 +10,37 @@ import re
 # wd pc fr nu cs qf sc fq zi dt nu fc - wdpcfrnucsqfscfqzidtnufc
 
 text = re.sub(r'\s+', '', text)
-print(text)
+
 for i in range(0, len(text), 2):
-    print(text[i]+text[i+1])
+    for j in range(5):
+        if text[i] in key[j]:
+            index12 = key[j].index(text[i])
+            index11 = j
+        if text[i+1] in key[j]:
+            index22 = key[j].index(text[i+1])
+            index21 = j
+    print(text[i]+text[i+1], index11, index12, ',', index21, index22)
+
+    if index11 != index21 and index12 != index22:
+        shifr = shifr + str(key[index21][index12]) + str(key[index11][index22])
+    elif index11 != index21 and index12 == index22:
+        if index11 == 4:
+            shifr = shifr + str(key[0][index12])
+        else:
+            shifr = shifr + str(key[index11 + 1][index12])
+        if index21 == 4:
+            shifr = shifr + str(key[0][index22])
+        else:
+            shifr = shifr + str(key[index21 + 1][index22])
+    elif index11 == index21 and index12 != index22:
+        if index12 == 4:
+            shifr = shifr + str(key[index11][0])
+        else:
+            shifr = shifr + str(key[index11][index12 + 1])
+        if index22 == 4:
+            shifr = shifr + str(key[index21][0])
+        else:
+            shifr = shifr + str(key[index21][index22 + 1])
+
+print(text)
+print(shifr)
